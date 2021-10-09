9414b6b19b4012560e61af17755977fc5aa5a260
wuguoxun <william.wu487@gmail.com>
2021-10-08 22:42:23 -0700

    Updates

---
+++ precache-manifest.0282b18de2b93507fac2558aa391a9d9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d320db2413a6146c5d1a5a4809968e",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "b90ef8e5d57bf95b7d83",
+    "url": "/keeper-app/static/js/2.2c826525.chunk.js"
+  },
+  {
+    "revision": "ea67db72ce288acd65ef",
+    "url": "/keeper-app/static/js/main.4ecf3526.chunk.js"
+  },
+  {
+    "revision": "fb7d45a05bf0dfe0fe92",
+    "url": "/keeper-app/static/js/runtime-main.42e7a143.js"
+  }
+]);
